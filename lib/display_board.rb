09fa8577b8ef5589961c191fc4209c1fcd602c72
Que' Manning <queisadiva@yahoo.com>
2020-11-12 04:44:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,10 @@
 def display_board
   puts "A Tic Tac Toe Board"
-  
   puts "Welcome to Tic Tac Toe"
+  puts "  |  |  |  "
+  puts "-----------"
+  puts "  |  |  |  "
+  puts "-----------"
   puts "  |  |  |  "
 end
 display_board
